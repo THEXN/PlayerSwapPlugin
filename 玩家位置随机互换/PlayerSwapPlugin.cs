b993fa9f0749197c305d48f723b085cd31fc26a8
xien <2383759126@qq.com>
2024-12-28 09:56:38 +0800

    计时器修改

--- 玩家位置随机互换/PlayerSwapPlugin.cs
+++ 玩家位置随机互换/PlayerSwapPlugin.cs
@@ -1,25 +1,24 @@
 ﻿using Microsoft.Xna.Framework;
-using System.Timers;
+using System;
+using System.Linq;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using TShockAPI.Hooks;
-
 
 namespace PlayerSwapPlugin
 {
     [ApiVersion(2, 1)]
     public class PlayerSwapPlugin : TerrariaPlugin
     {
-        private System.Timers.Timer timer;
-        private Random random;
         public override string Author => "肝帝熙恩,少司命";
         public override string Description => "一个插件，用于在指定时间后随机交换玩家位置。";
         public override string Name => "PlayerSwapPlugin";
         public override Version Version => new Version(1, 0, 5);
+
+        private DateTime LastCheck { get; set; } = DateTime.UtcNow;
+        private int RemainingSeconds { get; set; }
         public static Configuration Config;
-        // 在 PlayerSwapPlugin 类中定义一个实例字段
-        private System.Threading.Timer broadcastTimer;
 
         public PlayerSwapPlugin(Main game) : base(game)
         {
@@ -34,203 +33,63 @@ namespace PlayerSwapPlugin
 
         private void ReloadConfig(ReloadEventArgs args)
         {
-            // 重新加载配置文件
             LoadConfig();
             args.Player?.SendSuccessMessage("[{0}] 重新加载配置完毕。", typeof(PlayerSwapPlugin).Name);
-
-            // 更新广播剩余时间阈值
-            Config.BroadcastRemainingTimeThreshold = Config.BroadcastRemainingTimeThreshold; // 这里应该是从配置文件中读取的值
-
-            // 更新 broadcastTimer 的状态
-            this.broadcastTimer.Change(0, 1000);
+            RemainingSeconds = Config.IntervalSeconds;
         }
 
-        // 在方法外面定义一个TimerState类，用于存储倒计时的状态
-        public class TimerState
-        {
-            public int Countdown { get; set; } // 倒计时的秒数
-            public int Threshold { get; set; } // 广播的阈值
-            public TimerState(int countdown, int threshold)
-            {
-                Countdown = countdown;
-                Threshold = threshold;
-            }
-        }
         public override void Initialize()
         {
             GeneralHooks.ReloadEvent += ReloadConfig;
-            random = new Random();
-            timer = new System.Timers.Timer();
-            timer.Interval = TimeSpan.FromSeconds(Config.IntervalSeconds).TotalMilliseconds;
-            timer.AutoReset = true;
-            timer.Elapsed += Timer_Elapsed;
-            if (Config.pluginEnabled) // 检查插件是否启用
-            {
-                timer.Start(); // 如果启用，则启动定时器
-            }
-
             TShockAPI.Commands.ChatCommands.Add(new Command("swapplugin.toggle", SwapToggle, "swaptoggle", "随机互换"));
-            // 在 Initialize 方法中创建和初始化 broadcastTimer
-            broadcastTimer = new System.Threading.Timer(BroadcastMessage, new TimerState(Config.IntervalSeconds, Config.BroadcastRemainingTimeThreshold), Timeout.Infinite, 1000);
-        }
 
+            ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
 
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            var players = TShock.Players.Where(p => p != null && p.Active).ToList();
-            // 获取没有特定权限的玩家列表
-            var eligiblePlayers = players.Where(p => !p.HasPermission("noPlayerSwap")).ToList();
-
-            if (eligiblePlayers.Count < 2)
-                return;
-
-            if (!Config.pluginEnabled)
-                return;
-
-            SwapPlayers(); // 执行交换玩家位置的操作
-            timer.Interval = TimeSpan.FromSeconds(Config.IntervalSeconds).TotalMilliseconds;
-            // 重置System.Threading.Timer对象，让它开始倒计时
-            broadcastTimer.Change(0, 1000);
-        }//倒计时逻辑
-
-        private void BroadcastMessage(object state)
-        {
-            // 将state参数转换为TimerState对象
-            TimerState timerState = state as TimerState;
-            // 判断倒计时是否小于或等于阈值
-            if (timerState.Countdown <= timerState.Threshold && Config.BroadcastRemainingTimeEnabled)
-            {
-                // 向所有玩家发送一条消息，格式化倒计时
-                TShockAPI.TSPlayer.All.SendMessage($"注意：还有{timerState.Countdown}秒就要交换位置了！", Color.Yellow);
-            }
-            // 判断倒计时是否等于0
-            if (timerState.Countdown == 0)
-            {
-                // 停止计时器
-                broadcastTimer.Change(Timeout.Infinite, 1000);
-                // 重置倒计时
-                timerState.Countdown = Config.IntervalSeconds;
-                // 发送一条不同的消息，表示已经交换了所有玩家的位置
-                if (Config.BroadcastPlayerSwapEnabled)
-                {
-                    TShockAPI.TSPlayer.All.SendMessage($"已经交换所有玩家位置！", Color.Green);
-                }
-            }
-            else
-            {
-                // 将倒计时减1
-                timerState.Countdown--;
-            }
-        }//倒计时广播
-
-        private void SwapPlayers()
-        {
-            var players = TShock.Players.Where(p => p != null && p.Active).ToList();
-            // 获取没有特定权限的玩家列表
-            var eligiblePlayers = players.Where(p => !p.HasPermission("noPlayerSwap")).ToList();
-
-            if (eligiblePlayers.Count < 2)
-                return;
-
-            if (Config.MultiPlayerMode)
-            {
-                // 多人打乱模式逻辑
-                PlayerRandPos3(eligiblePlayers);
-            }
-            else
-            {
-                // 双人模式逻辑
-                SwapTwoPlayers(eligiblePlayers);
-
-            }
-        }//根据配置文件选择模式
-
-        private void SwapTwoPlayers(List<TSPlayer> players)
-        {
-            int index1 = random.Next(players.Count);
-            int index2 = random.Next(players.Count);
-
-            while (!Config.AllowSamePlayerSwap && index2 == index1)
-            {
-                index2 = random.Next(players.Count);
-            }
-
-            var player1 = players[index1];
-            var player2 = players[index2];
-
-            // 检查是否为同一玩家
-            if (!Config.AllowSamePlayerSwap && player1 == player2)
-            {
-                player1.SendInfoMessage("你尝试和自己交换位置，但没有发生任何变化。");
-                return;
-            }
-
-            SwapPositions(player1, player2);
-
-            // 发送交换提示
-            player1.SendInfoMessage($"你已与玩家 {player2.Name} 交换了位置！");
-            player2.SendInfoMessage($"你已与玩家 {player1.Name} 交换了位置！");
-
-            // 广播玩家交换位置信息
-            if (Config.BroadcastPlayerSwapEnabled)
-            {
-                TShockAPI.TSPlayer.All.SendMessage($"玩家 {player1.Name} 和玩家 {player2.Name} 交换了位置！", Color.Green);
-            }
-        }//仅交换双人模式
-        private void SwapPositions(TSPlayer player1, TSPlayer player2)
-        {
-            // 保存 player1 当前位置
-            int tempX1 = player1.TileX;
-            int tempY1 = player1.TileY;
-
-            // 保存 player2 当前位置
-            int tempX2 = player2.TileX;
-            int tempY2 = player2.TileY;
-
-            // 传送 player1 至 player2 的位置
-            player1.Teleport(tempX2 * 16, tempY2 * 16);
-
-            // 传送 player2 至 player1 的位置
-            player2.Teleport(tempX1 * 16, tempY1 * 16);
-        }//双人模式,交换逻辑
-
-        public void PlayerRandPos3(List<TSPlayer> players)
-        {
-            // 接受一个玩家列表作为参数，而不是直接获取所有玩家
-            if (players.Count < 2)
-                return;
-
-            // 随机打乱玩家列表的顺序
-            var sp = players.OrderBy(c => Guid.NewGuid()).ToList();
-
-            // 用一个for循环，每次取出两个玩家，交换他们的位置
-            for (var i = 1; i < players.Count; i++)
-            {
-                (sp[i - 1].TPlayer.position, sp[i].TPlayer.position) =
-                (sp[i].TPlayer.position, sp[i - 1].TPlayer.position);
-            }
-
-            // 对每个玩家，传送他们到新的位置
-            foreach (var player in players)
-            {
-                player.Teleport(player.TPlayer.position.X, player.TPlayer.position.Y);
-            }
+            RemainingSeconds = Config.IntervalSeconds;
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                if (timer != null)
-                {
-                    timer.Stop();
-                    timer.Dispose();
-                    timer = null;
-                }
+                ServerApi.Hooks.GameUpdate.Deregister(this, OnUpdate);
             }
             base.Dispose(disposing);
         }
 
+        private void OnUpdate(EventArgs args)
+        {
+            if (!Config.pluginEnabled)
+            {
+                RemainingSeconds = Config.IntervalSeconds;
+                return;
+            }
+
+            var players = TShock.Players.Where(p => p != null && p.Active && !p.Dead).ToList();
+            var eligiblePlayers = players.Where(p => !p.HasPermission("noplayerswap")).ToList();
+
+            if (eligiblePlayers.Count < 2)
+            {
+                RemainingSeconds = Config.IntervalSeconds;
+                return;
+            }
+
+            if ((DateTime.UtcNow - this.LastCheck).TotalSeconds >= 1)
+            {
+                this.LastCheck = DateTime.UtcNow;
+                RemainingSeconds--;
+
+                if (RemainingSeconds <= 0)
+                {
+                    SwapPlayers(eligiblePlayers);
+                    RemainingSeconds = Config.IntervalSeconds;
+                }
+                else if (Config.BroadcastRemainingTimeEnabled && RemainingSeconds <= Config.BroadcastRemainingTimeThreshold)
+                {
+                    TShockAPI.TSPlayer.All.SendMessage($"注意：还有{RemainingSeconds}秒就要交换位置了！", Color.Yellow);
+                }
+            }
+        }
         private void SwapToggle(CommandArgs args)
         {
             if (args.Parameters.Count < 1)
@@ -286,6 +145,83 @@ namespace PlayerSwapPlugin
                     args.Player.SendErrorMessage("用法: /随机互换 <广播时间|广播交换|开关|传送间隔|和自己交换");
                     break;
             }
+        }
+
+        private void SwapPlayers(List<TSPlayer> players)
+        {
+            if (Config.MultiPlayerMode)
+            {
+                SwapMorePlayers(players);
+            }
+            else
+            {
+                SwapTwoPlayers(players);
+            }
+        }
+        private void SwapTwoPlayers(List<TSPlayer> players)
+        {
+            Random random = new Random();
+            int index1 = random.Next(players.Count);
+            int index2 = random.Next(players.Count);
+
+            while (!Config.AllowSamePlayerSwap && index2 == index1)
+            {
+                index2 = random.Next(players.Count);
+            }
+
+            var player1 = players[index1];
+            var player2 = players[index2];
+
+            if (!Config.AllowSamePlayerSwap && player1 == player2)
+            {
+                player1.SendMessage("你尝试和自己交换位置，但没有发生任何变化。", Color.Blue);
+                return;
+            }
+
+            SwapPositions(player1, player2);
+
+            player1.SendMessage($"你已与玩家 {player2.Name} 交换了位置！", Color.Blue);
+            player2.SendMessage($"你已与玩家 {player1.Name} 交换了位置！", Color.Blue);
+
+            if (Config.BroadcastPlayerSwapEnabled)
+            {
+                TShockAPI.TSPlayer.All.SendMessage($"玩家 {player1.Name} 和玩家 {player2.Name} 交换了位置！", Color.Yellow);
+            }
+        }
+
+        private void SwapMorePlayers(List<TSPlayer> players)
+        {
+            if (players.Count < 2)
+                return;
+
+            var sp = players.OrderBy(c => Guid.NewGuid()).ToList();
+
+            for (var i = 1; i < players.Count; i++)
+            {
+                (sp[i - 1].TPlayer.position, sp[i].TPlayer.position) =
+                (sp[i].TPlayer.position, sp[i - 1].TPlayer.position);
+            }
+
+            foreach (var player in players)
+            {
+                player.Teleport(player.TPlayer.position.X, player.TPlayer.position.Y);
+            }
+
+            if (Config.BroadcastPlayerSwapEnabled)
+            {
+                TShockAPI.TSPlayer.All.SendMessage($"已经交换所有玩家位置！", Color.Green);
+            }
+        }
+        private void SwapPositions(TSPlayer player1, TSPlayer player2)
+        {
+            int tempX1 = player1.TileX;
+            int tempY1 = player1.TileY;
+
+            int tempX2 = player2.TileX;
+            int tempY2 = player2.TileY;
+
+            player1.Teleport(tempX2 * 16, tempY2 * 16);
+            player2.Teleport(tempX1 * 16, tempY1 * 16);
         }
 
     }
